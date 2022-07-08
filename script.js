661f60564690d320b90b72d3411a96a8afacc36c
keonp <keonlmp@gmail.com>
2022-07-07 21:03:27 -0400

    created a function to change menu button appreance when clicked

--- script.js
+++ script.js
@@ -7,11 +7,21 @@ const hamburgerMenu = document.querySelector('.hamburgerMenu');
 
 
 hamburgerMenu.addEventListener('click', function(event) {
-    console.log(event.target.children);
-    // .children = children
+    // Store event in a variable
+    let menuBars = event.target;
+
+    // In the event that a span is selected instead of the div and store in the variable above, convert the variable to the parent element - aka the div
+    if (menuBars.tagName === 'SPAN') {
+        menuBars = menuBars.parentElement;
+    }
+
+    menuBars = menuBars.children;
+
+    const topBar = menuBars['0'];
+    const middleBar = menuBars['1'];
+    const bottomBar = menuBars['2'];
+
+    topBar.classList.toggle('topMenuBar');
+    middleBar.classList.toggle('middleMenuBar');
+    bottomBar.classList.toggle('bottomMenuBar');
 })
-
-
-// hamburgerMenu.addEventListener('click', function(event) {
-//     console.log(event);
-// })
